be24379ac4271198529a70096222cbfaa13ec5f7
Nathan Le Bigot <nathan.le-bigot@epitech.eu>
2022-10-18 18:15:33 +0200

    actions(kubeconform): change file ptr to string

--- kubeconform.go
+++ kubeconform.go
@@ -24,21 +24,20 @@ func main() {
         if file.Mode().IsRegular() {
             if filepath.Ext(file.Name()) == ".yaml" {
                 fmt.Println(file.Name())
-                file := "cluster-architecture.yml"
-                f, err := os.Open(file)
+                f, err := os.Open(file.Name())
                 if err != nil {
-                    log.Fatalf("failed opening %s: %s", file, err)
+                    log.Fatalf("failed opening %s: %s", file.Name(), err)
                 }
                 v, err := validator.New(nil, validator.Opts{Strict: true})
                 if err != nil {
                     log.Fatalf("failed initializing validator: %s", err)
                 }
-                for i, res := range v.Validate(file, f) {
+                for i, res := range v.Validate(file.Name(), f) {
                     if res.Status == validator.Invalid {
-                        log.Fatalf("resource %d in file %s is not valid: %s", i, file, res.Err)
+                        log.Fatalf("resource %d in file %s is not valid: %s", i, file.Name(), res.Err)
                     }
                     if res.Status == validator.Error {
-                        log.Fatalf("error while processing resource %d in file %s: %s", i, file, res.Err)
+                        log.Fatalf("error while processing resource %d in file %s: %s", i, file.Name(), res.Err)
                     }
                 }
             }
